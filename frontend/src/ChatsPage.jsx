a3a67c8a1238193e6d31940702918aba50eddacb
Esteban Castaño Rivera <estebancastriver@gmail.com>
2024-03-16 12:36:36 -0500

    .gitignore modified

---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+const ChatsPage = (props) => {
+    return (
+        <div className="background">
+            <PrettyChatWindow
+                projectId={process.env.REACT_APP_CHAT_ENGINE_PROJECT_ID}
+                username={props.user.username}
+                secret={props.user.secret}
+            />
+        </div>
+    );
+};
+export default ChatsPage;
